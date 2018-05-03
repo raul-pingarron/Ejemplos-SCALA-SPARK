add131ddebf7e3a5b6fb6b651e9d22cc8c881a34
Raul Pingarron <raul.ping4rr0n@gmail.com>
2018-05-04 00:40:55 +0200

    Add files via upload

---
+++ GeneraDatosDL/GeneraDatos.sbt
@@ -0,0 +1,26 @@
+name := "GeneraDatos"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.2.0",
+  "org.apache.spark" %% "spark-mllib" % "2.2.0",
+)
+
+resolvers ++= Seq(
+  "JBoss Repository" at "http://repository.jboss.org/nexus/content/repositories/releases/",
+  "Spray Repository" at "http://repo.spray.cc/",
+  "Cloudera Repository" at "https://repository.cloudera.com/artifactory/cloudera-repos/",
+  "Akka Repository" at "http://repo.akka.io/releases/",
+  "Twitter4J Repository" at "http://twitter4j.org/maven2/",
+  "Apache HBase" at "https://repository.apache.org/content/repositories/releases",
+  "Twitter Maven Repo" at "http://maven.twttr.com/",
+  "scala-tools" at "https://oss.sonatype.org/content/groups/scala-tools",
+  "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
+  "Second Typesafe repo" at "http://repo.typesafe.com/typesafe/maven-releases/",
+  "Mesosphere Public Repository" at "http://downloads.mesosphere.io/maven",
+  Resolver.sonatypeRepo("public")
+)
+
---
+++ GeneraDatosDL/src/main/scala/GeneraDatos.scala
@@ -0,0 +1,50 @@
+/** Programa para generar ficheros vectoriales dispersos */
+/** R.Pingarron <raul@purestorage.com> */
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.spark.mllib.random._
+import org.apache.spark.rdd.{PairRDDFunctions, RDD}
+import org.apache.spark.mllib.linalg.{Vectors, Vector}
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
+
+object GeneraDatos {
+
+  def main(args: Array[String]) {
+
+  /**  Eliminamos logs innecesarios: */
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    if (args.length < 1) {
+      System.err.println("Uso: GeneraDatos <RUTA-NFS>")
+      System.exit(1)
+    }
+
+    val conf = new SparkConf().setAppName("GeneraDatos")
+    val sc = new SparkContext(conf)
+
+    var numFilas: Int = 40000000
+    var numColumnas: Int = 2048
+    var numParticiones: Int = 100000
+    var dispersidad: Double = 0.5
+
+    val rdd1: RDD[Vector] = RandomRDDs.normalVectorRDD(sc, numFilas, numColumnas, numParticiones)
+    val rango = new java.util.Random()
+    val data = rdd1.map{v =>
+      val a = Array.fill[Double](v.size)(0.0)
+      v.foreachActive{(i,vi) =>
+         if(rango.nextDouble <= dispersidad){
+           a(i) = vi
+         }
+      }
+      Vectors.dense(a).toSparse
+   }
+
+    data.saveAsObjectFile(args(0))
+
+    sc.stop()
+  }
+}
